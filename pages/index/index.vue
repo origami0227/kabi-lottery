511c8be08a1ab0ab3875b21ce913ac7127859dc0
origami <1071242996@qq.com>
2023-02-08 09:42:30 +0800

    update:完成抽奖UI

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,12 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.svg"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<!-- <image class="logo" src="/static/logo.svg"></image> -->
+		<view class="panel">
+			<text>{{num}}</text>
+		</view>
+		<view class="button-area">
+			<button v-show="isOver">开始抽奖</button>
+			<button v-show="!isOver">结束抽奖</button>
 		</view>
 	</view>
 </template>
@@ -11,7 +15,9 @@
 	export default {
 		data() {
 			return {
-				title: '卡比抽奖'
+				title: '卡比抽奖',
+				num: 40,
+				isOver: true,
 			}
 		},
 		onLoad() {
@@ -23,30 +29,31 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.panel {
+		width: 600rpx;
+		height: 400rpx;
+		border: 1rpx solid $uni-border-color;
+		border-radius: 8rpx;
+		margin-top: 60rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		box-shadow: 0 0 40rpx 0rpx rgba(0, 0, 0, 0.1);
+
+		text {
+			font-size: 180rpx;
+			
+		}
+	}
+	.button-area{
+		margin-top: 120rpx;
+	}
+
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
 	}
 </style>
