121115d0b8da4ed85d7c8ac2d2ab4cbfa4e25a60
origami <1071242996@qq.com>
2023-02-09 11:09:10 +0800

    update:设置页面UI完成

--- pages/setting/setting.vue
+++ pages/setting/setting.vue
@@ -1,6 +1,22 @@
 <template>
-	<view>
-		设置
+	<view class="content">
+		<view class="list">
+			<view class="uni-form-item uni-column">
+				<view class="title">最大值</view>
+				<input class="uni-input" type="number" placeholder="最大值" />
+			</view>
+			<view class="uni-form-item uni-column">
+				<view class="title">最小值</view>
+				<input class="uni-input" type="number" placeholder="最小值" />
+			</view>
+			<view class="uni-form-item uni-column">
+				<view class="title">时间间隔（ms）</view>
+				<input class="uni-input" type="number" placeholder="时间间隔" />
+			</view>
+		</view>
+		<view class="button-area">
+			<button>保存</button>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +24,36 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.content {
+		padding: 40rpx;
+	}
 
+	.uni-form-item {
+		margin-top: 20rpx;
+	}
+	.uni-input{
+		border-bottom: 1rpx solid $uni-border-color;
+		font-size: 26rpx;
+		margin-top: 16rpx;
+	}
+	.button-area{
+		margin-top: 120rpx;
+		display: flex;
+		justify-content: center;
+		text-align: center;
+		
+		button {
+			width: 400rpx;
+		}
+	}
 </style>
