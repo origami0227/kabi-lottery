2716c51ba7da4bbafb8b695314e8cdeb84dc312b
origami <1071242996@qq.com>
2023-02-09 12:58:24 +0800

    update:完成设置功能

--- pages/setting/setting.vue
+++ pages/setting/setting.vue
@@ -3,19 +3,19 @@
 		<view class="list">
 			<view class="uni-form-item uni-column">
 				<view class="title">最大值</view>
-				<input class="uni-input" type="number" placeholder="最大值" />
+				<input class="uni-input" v-model="max" type="number" placeholder="最大值" />
 			</view>
 			<view class="uni-form-item uni-column">
 				<view class="title">最小值</view>
-				<input class="uni-input" type="number" placeholder="最小值" />
+				<input class="uni-input" v-model="min" type="number" placeholder="最小值" />
 			</view>
 			<view class="uni-form-item uni-column">
 				<view class="title">时间间隔（ms）</view>
-				<input class="uni-input" type="number" placeholder="时间间隔" />
+				<input class="uni-input" v-model="interval" type="number" placeholder="时间间隔" />
 			</view>
 		</view>
 		<view class="button-area">
-			<button>保存</button>
+			<button @click="onSave">保存</button>
 		</view>
 	</view>
 </template>
@@ -24,11 +24,35 @@
 	export default {
 		data() {
 			return {
-
+				max: 100,
+				min: 0,
+				interval: 100, //ms
+			}
+		},
+		created() {
+			console.log('created')
+			try {
+				let setting = uni.getStorageSync('setting') || {
+					max: 100,
+					min: 0,
+					interval: 100
+				} //一进入页面就提取保存的setting，如果没有setting就只用默认值
+				this.max = setting.max
+				this.min = setting.min
+				this.interval = setting.interval
+			} catch (e) {
+				console.log(e)
 			}
 		},
 		methods: {
-
+			onSave() { //本地存储
+				let setting = {
+					max: this.max,
+					min: this.min,
+					interval: this.interval
+				} //保存为一个对象
+				uni.setStorageSync('setting', setting)
+			}
 		}
 	}
 </script>
@@ -41,17 +65,19 @@
 	.uni-form-item {
 		margin-top: 20rpx;
 	}
-	.uni-input{
+
+	.uni-input {
 		border-bottom: 1rpx solid $uni-border-color;
 		font-size: 26rpx;
 		margin-top: 16rpx;
 	}
-	.button-area{
+
+	.button-area {
 		margin-top: 120rpx;
 		display: flex;
 		justify-content: center;
 		text-align: center;
-		
+
 		button {
 			width: 400rpx;
 		}
