a0228c13cae660b8e2dcf4bb470032de92634ddd
origami <1071242996@qq.com>
2023-03-01 14:20:12 +0800

    优化图片

--- pages/lottery/list.vue
+++ pages/lottery/list.vue
@@ -10,6 +10,7 @@
                 <!-- 此处默认显示为_id，请根据需要自行修改为其他字段 -->
                 <!-- 如果使用了联表查询，请参考生成的 admin 项目中 list.vue 页面 -->
                 {{item.title}}
+				<image :src="item.photo.url" mode="aspectFill"></image>
               </text>
             </template>
           </uni-list-item>
